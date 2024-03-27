c1712eb389d2e9a882ce91cae6ba0acbb8ccd91e
James Griffiths <james@jwgsoftware.com>
2024-03-27 17:42:25 +0000

    EHD-959: Re-publish WBC website: Shell script (wget) to create offline copy of website

---
+++ save-offline-copy-of-website.sh
@@ -0,0 +1,10 @@
+wget \
+    --recursive \
+    --level=inf \
+    --page-requisites \
+    --adjust-extension \
+    --convert-links \
+    --restrict-file-names=windows \
+    --domains=localhost \
+    --directory-prefix=offline-copy-of-website
+    http://localhost:8080/
