676c1dfa880eaad82864e7a4fceea1b206adb6e2
James Griffiths <james@jwgsoftware.com>
2025-02-17 12:35:52 +0000

    EHD-1567: New WBC website: Remove old website: Misc

--- save-offline-copy-of-website.sh
+++
@@ -1,9 +0,0 @@
-wget \
-    --recursive \
-    --level=inf \
-    --page-requisites \
-    --adjust-extension \
-    --restrict-file-names=windows \
-    --domains=localhost \
-    --directory-prefix=offline-copy-of-website \
-    http://localhost:8080/
