f9ee03a6d370e113a8ae235101db67cb091ee255
James Griffiths <james@jwgsoftware.com>
2025-02-17 17:27:25 +0000

    EHD-1567: New WBC website: Eleventy configuration

---
+++ eleventy.config.js
@@ -0,0 +1,54 @@
+const govukEleventyPlugin = require('@x-govuk/govuk-eleventy-plugin')
+
+module.exports = function(eleventyConfig) {
+    // Register the plugin
+    eleventyConfig.addPlugin(govukEleventyPlugin, {
+        icons: {
+            mask: false,
+            shortcut: false,
+            touch: false,
+        },
+        showBreadcrumbs: false,
+        titleSuffix: "Women's Business Council",
+        header: {
+            logotype: {
+                text: "Women's Business Council"
+            },
+        },
+        navigation: {
+            items: [
+                {
+                    text: 'Home',
+                    href: '/',
+                },
+                {
+                    text: 'About',
+                    href: '/about',
+                },
+                {
+                    text: 'Members',
+                    href: '/members',
+                },
+                {
+                    text: 'Resources',
+                    href: '/resources',
+                },
+            ],
+        },
+        footer: {
+            copyright: false,
+        },
+    })
+
+    return {
+        dataTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+        markdownTemplateEngine: 'njk',
+        dir: {
+            // The folder where all your content will live:
+            input: 'app',
+            // Use layouts from the plugin
+            layouts: '../node_modules/@x-govuk/govuk-eleventy-plugin/layouts'
+        },
+    }
+};
